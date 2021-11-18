e0d0963a1b9d9d1b9b1e9ee144e5e8ce4f71be72
Egor <rodionegor31@gmail.com>
2021-11-18 15:39:04 +0300

    HomeWork5
    Всё сделал и так же добавил проверку на существование чата.

--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleCheckbox } from "../../store/profile/actions";
 
 export const Profile = () => {
+    const checkboxValue = useSelector((state) => state.checkbox);
+    const name = useSelector((state) => state.name);
+    const dispatch = useDispatch();
+
+    const handleChange = () => {
+        dispatch(toggleCheckbox);
+    };
+
     return (
         <div>
             <h2>Profile</h2>
+            <input
+                type="checkbox"
+                checked={checkboxValue}
+                onChange={handleChange}
+            />
+            <span>{name}</span>
         </div>
     );
 };
