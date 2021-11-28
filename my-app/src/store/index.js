d4b2b85632cae4f96468401baf8db1a18a01d3d3
Egor <rodionegor31@gmail.com>
2021-11-29 02:30:19 +0300

    persist
    Установил и настроил redux-persist

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,16 +1,32 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+
 import { chatsReducer } from "./chats/reducer";
 import { profileReducer } from "./profile/reducer";
 import { messagesReducer } from "./messages/reducer";
+
 import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+
+import { persistReducer, persistStore } from "redux-persist";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(
+const config = {
+    key: "gbMsngr",
+    storage,
+};
+
+const persistedReducer = persistReducer(
+    config,
     combineReducers({
         chats: chatsReducer,
         profile: profileReducer,
         messages: messagesReducer,
-    }),
+    })
+);
+
+export const store = createStore(
+    persistedReducer,
     composeEnhancers(applyMiddleware(thunk))
 );
+export const persistor = persistStore(store);
