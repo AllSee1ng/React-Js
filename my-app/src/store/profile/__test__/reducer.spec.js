2697cdead37f727209beca5435c55ff1d96b9fc5
Egor <rodionegor31@gmail.com>
2021-12-06 16:55:03 +0300

    HomeWork 10

---
+++ my-app/src/store/profile/__test__/reducer.spec.js
@@ -0,0 +1,36 @@
+import { toggleCheckbox } from "../actions";
+import { signIn } from "../actions";
+import { signOut } from "../actions";
+import { profileReducer } from "../reducer";
+
+describe("testing profile reducer", () => {
+    const initialState = {
+        checkbox: false,
+        name: "User",
+        authed: false,
+    };
+
+    it("toggle checkbox", () => {
+        const expected = {
+            checkbox: true,
+        };
+        const received = profileReducer(initialState, toggleCheckbox);
+        expect(received.checkbox).toEqual(expected.checkbox);
+    });
+
+    it("sign in", () => {
+        const expected = {
+            authed: true,
+        };
+        const received = profileReducer(initialState, signIn());
+        expect(received.authed).toEqual(expected.authed);
+    });
+
+    it("sign out", () => {
+        const expected = {
+            authed: false,
+        };
+        const received = profileReducer(initialState, signOut());
+        expect(received.authed).toEqual(expected.authed);
+    });
+});
